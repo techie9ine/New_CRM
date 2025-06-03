ca0d499dcbf94b4b4ae339f7fcc9bfa15c5cc1fa
techie9ine <eokpala999@gmail.com>
2025-06-03 10:46:39 +0100

    Add Render domain to ALLOWED_HOSTS

--- crm_prj/settings.py
+++ crm_prj/settings.py
@@ -31,8 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ["new-crm.onrender.com", "127.0.0.1"]
-
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0", "new-crm-app.onrender.com"]
 
 # Application definition
 
