d496d4dc3c440533473413d5edaa6a5b90bf0de7
techie9ine <eokpala999@gmail.com>
2025-06-03 11:21:30 +0100

    Added build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
-# Exit on error
+
 set -o errexit
 
-# Install Python dependencies
 pip install -r requirements.txt
-
-# Collect static files (crucial for CSS/JS/images in production)
-python manage.py collectstatic --no-input
-
-# Apply database migrations (CRITICAL for creating tables like auth_user)
 python manage.py migrate
+python manage.py collectstatic --noinput # --noinput avoids interactive prompts
